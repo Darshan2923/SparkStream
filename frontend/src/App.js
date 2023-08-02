59766ae94ee2a313e9362dc16fc25cf463b6ba71
Darshan <darshanpatel2923@gmail.com>
2023-08-02 17:27:16 +0530

    Added react router dom in site for multiple page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Chats from './components/Chats';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      {/* Remember the syntax has changed in recent react-router-dom version 6 
+      You need to wrap all in <BrowserRouter,Routes then use element={} inside the <Route> to point the component
+      for more doubts visit: https://stackoverflow.com/questions/69832748/error-error-a-route-is-only-ever-to-be-used-as-the-child-of-routes-element/69837100#69837100?newreg=9735e4bf07b64ef19dd03e522d559cfa
+      */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/chats' element={<Chats />} />
+      </Routes>
     </div>
   );
 }
