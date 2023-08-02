1df2f623e928ebef1170cb2549de2af0e2d50fba
Darshan <darshanpatel2923@gmail.com>
2023-08-02 22:40:22 +0530

    Added the ui, will do it later

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Container, Box, Text, TabList, Tabs, Tab, TabPanels, TabPanel } from '@chakra-ui/react'
+import SignUp from './SignUp'
+import Login from './Login'
 
 function Home() {
     return (
-        <div>Home</div>
+        <Container maxW='xl' centerContent className='home-container'>
+            <Box
+                m="40px 0 15px 0"
+            >
+                <Text
+                    fontSize="4xl"
+                    fontFamily="Space Grotesk"
+                    color="white">SparkSense</Text>
+            </Box>
+            <Box bg='white' w='100%' p={4} borderRadius='lg' color='black' borderWidth='1px'>
+                <Tabs variant='soft-rounded' colorScheme='green'>
+                    <TabList mb='1em'>
+                        <Tab width='50%'>SignUp</Tab>
+                        <Tab width="50%">Login</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <SignUp />
+                        </TabPanel>
+                        <TabPanel>
+                            <Login />
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </Box>
+        </Container >
     )
 }
 
