35c535a8a5a6de3f5598fd2458da9f5c7e474772
Darshan <darshanpatel2923@gmail.com>
2023-08-03 18:28:00 +0530

    errorHandlling in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 const chats = require('./data/data');
 const connectDB = require("./config/db");
 const colors = require("colors");
-const userRoutes = require('./routes/userRoutes')
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware.js/errorMiddleware");
 
 //Now create instance of this express variable
 dotenv.config();
@@ -41,7 +42,10 @@ app.get("/api/chat/:id", (req, res) => {
 
 // Adding an endpoint for user authentication
 
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT
 // https://stackoverflow.com/questions/9164915/node-js-eacces-error-when-listening-on-most-ports
