b06a3de95a56863e9066b4f36c573fe1d24a9840
Darshan <darshanpatel2923@gmail.com>
2023-08-03 11:08:25 +0530

    Connected mongoDB to our server successfully

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require('dotenv');
-const chats = require('./data/data')
+const chats = require('./data/data');
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 //Now create instance of this express variable
 dotenv.config();
+connectDB();
 const app = express();
 
 // Now we can use this app variable to create a server
@@ -35,4 +38,4 @@ app.get("/api/chat/:id", (req, res) => {
 const PORT = process.env.PORT
 // https://stackoverflow.com/questions/9164915/node-js-eacces-error-when-listening-on-most-ports
 
-app.listen(5000, console.log(`Server is running`));
+app.listen(5000, console.log(`Server is running`.yellow.bold));
